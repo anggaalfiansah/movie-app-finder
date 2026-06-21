4a11a0c3838b2b29b39d6744f5aefd920ecbe04d
anggaalfiansah <rajacapybara275@gmail.com>
2026-06-21 21:31:14 +0700

    fix: conditionally set base path for deployment environments in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-app-finder/',
+  base: process.env.GITHUB_ACTIONS ? '/movie-app-finder/' : '/',
 })
